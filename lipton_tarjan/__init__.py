6ce6cfc7f79f262bec16005065c1413593b02400
Valerii Likhosherstov <v.lihosherstov@gmail.com>
2022-08-26 12:26:29 +0100

    Removed numba from __init__.py

--- lipton_tarjan/__init__.py
+++ lipton_tarjan/__init__.py
@@ -1,5 +1,5 @@
-from .planar_graph_edges import PlanarGraphEdges, planar_graph_edges_nb_type
-from .planar_graph import PlanarGraph, planar_graph_nb_type
+from .planar_graph_edges import PlanarGraphEdges
+from .planar_graph import PlanarGraph
 from . import triangulator, planar_separator, planar_graph_constructor, separation_class
 from .triangulator import Triangulator
 from .planar_graph_generator import PlanarGraphGenerator
